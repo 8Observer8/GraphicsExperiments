4ea1b2d233a1b79b5f057df35136436e23088032
Omar Huseynov <omarhuseynov69@gmail.com>
2016-11-01 18:28:23 +0300

    First Commit

---
+++ Default/Scripts/JS/default.js
@@ -0,0 +1,34 @@
+/* Canvas element */
+var CANVAS = document.createElement("canvas");
+/* WebGL context */
+var GL = CANVAS.getContext("webgl");
+document.body.appendChild(CANVAS);
+/**
+ * Resizes the context
+ *
+ * @returns void
+ */
+function Resize() {
+    CANVAS.width = window.innerWidth;
+    CANVAS.height = window.innerHeight;
+    GL.viewport(0, 0, CANVAS.width, CANVAS.height);
+}
+window.addEventListener("resize", Resize);
+/**
+ * Main function
+ *
+ * @returns void
+ */
+function Main() {
+    if (GL === null) {
+        throw new Error("WebGL is not supported");
+    }
+    else {
+        CANVAS.width = window.innerWidth;
+        CANVAS.height = window.innerHeight;
+        GL.clearColor(0.5, 0.5, 1.0, 1.0);
+        GL.viewport(0, 0, CANVAS.width, CANVAS.height);
+        GL.clear(GL.COLOR_BUFFER_BIT);
+    }
+}
+Main();
